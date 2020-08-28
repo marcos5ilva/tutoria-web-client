f6fd5991e6d15caec553e888f7946333ad397108
Marcos Silva <marcos.silva79@gmail.com>
2020-08-28 18:39:27 -0400

    feat: add TutorItem contact functionality

--- src/components/TutorItem/index.tsx
+++ src/components/TutorItem/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 import './styles.css';
+import api from '../../services/api';
 
 export interface Tutor{
-    
+        id: number;
         avatar: string
         bio: string;
         cost: number;
@@ -16,7 +17,16 @@ interface TutorItemProps{
     tutor: Tutor;
 }
 
+
+
 const TutorItem: React.FC <TutorItemProps>= ({tutor}) => {
+
+    function createNewConnection(){
+        api.post('connections', {
+            user_id: tutor.id
+        })
+    }
+
     return (<article className="tutor-item">
         <header>
             <img src={tutor.avatar} alt={tutor.name} />
@@ -28,9 +38,12 @@ const TutorItem: React.FC <TutorItemProps>= ({tutor}) => {
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum similique eos perspiciatis eius, voluptas dicta ut iure porro provident nemo beatae! Aliquid quod voluptate corrupti ullam minus, expedita totam placeat?</p>
         <footer><p>Hourly Price:</p>
         <strong>${tutor.cost}</strong>
-            <button type="button">
+            <a  onClick={createNewConnection} 
+            href={`https://wa.me/${tutor.whatsapp}`} 
+            type="button" 
+            target= "blank">
                 <img src={whatsappIcon} alt="whatsapp icon" />
-                contact me</button>
+                contact me</a>
         </footer>
     </article>)
 }
